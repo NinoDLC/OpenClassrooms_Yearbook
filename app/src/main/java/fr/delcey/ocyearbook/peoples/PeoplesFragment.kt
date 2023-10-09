fdff4a531fe0bb28321296236568e6f5bb027914
davidvignon <davidvignon86@yahoo.fr>
2023-10-09 10:59:51 +0200

    Add people object and recyclerview + david account

--- app/src/main/java/fr/delcey/ocyearbook/peoples/PeoplesFragment.kt
+++ app/src/main/java/fr/delcey/ocyearbook/peoples/PeoplesFragment.kt
@@ -5,24 +5,41 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import fr.delcey.ocyearbook.R
-import fr.delcey.ocyearbook.databinding.PeoplesFragmentBinding
+import fr.delcey.ocyearbook.data.People
+import fr.delcey.ocyearbook.databinding.PeopleFragmentBinding
 import fr.delcey.ocyearbook.utils.viewBinding
 
-class PeoplesFragment : Fragment(R.layout.peoples_fragment) {
+class PeoplesFragment : Fragment(R.layout.people_fragment) {
+
+
     companion object {
         fun newInstance() = PeoplesFragment().apply {
             arguments = Bundle()
         }
     }
 
-    private val binding by viewBinding { PeoplesFragmentBinding.bind(it) }
+    private val binding by viewBinding { PeopleFragmentBinding.bind(it) }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.peoplesCardViewNino.setOnClickListener {
+        val adapter = PeopleAdapter(requireContext())
+
+        binding.peopleRecyclerView.adapter = adapter
+
+        binding.peopleRecyclerView.setOnClickListener {
             Toast.makeText(requireContext(), getNinoToast(), Toast.LENGTH_LONG).show()
         }
+
+        val peopleList: MutableList<People> =
+            mutableListOf(
+                People(0,(R.drawable.nino), "Nino", "Mentor", "Kotlin", "Java",
+                    R.string.nino_toast
+                ),
+                People(1,(R.drawable.david), "David", "Student", "Kotlin", "Java", R.string.david_toast)
+            )
+
+        adapter.submitList(peopleList)
     }
 
     private fun getNinoToast(): CharSequence = getText(R.string.nino_toast)
